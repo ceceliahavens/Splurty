617102695276e2af110efcb4deb29a8df6bbcdca
ceceliahavens <ceceliahavens@hotmail.com>
2015-09-18 01:13:45 +0000

    lobster fonts

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,10 @@ module Splurty
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     # Add the fonts path
-    config.assets.path << "#{Rails.root}/app/assets/fonts"
+    # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
-    #Precompile additional assets
+    # Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
